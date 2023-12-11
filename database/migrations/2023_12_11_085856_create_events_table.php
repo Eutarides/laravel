9fb7036e7d3980ab1d3af9c38ca4221efb00f990
Eutarides <javifarmacastillo@gmail.com>
2023-12-11 12:21:21 +0100

    migraciones

--- database/migrations/2023_12_11_085856_create_events_table.php
+++ database/migrations/2023_12_11_085856_create_events_table.php
@@ -11,13 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('address')->default("Evento online");
+            $table->varchar('description');
+            $table->date('startDate');
+            $table->date('endDate');
+            $table->time('startTime');
+            $table->time('endTime');
+            $table->decimal('price',8,2)->default(0);
             $table->timestamps();
             $table->softDelete();
         });
@@ -28,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('events');
     }
 };
